d52ab8e9fe14588d40cee9e086f99bf7bd19ac0d
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-11-07 21:53:59 +0300

    init: start freecodecamp course

---
+++ while_loops/iterate_with_javascript_while_loops.js
@@ -0,0 +1,14 @@
+// Setup
+let myArray = [];
+let i = 5;
+
+while (i >= 0) {
+    myArray = [...myArray, i];
+    i--
+}
+console.log(`myArray: ${myArray}`);
+console.log("myArray:", myArray);
+console.log(`typeof myArray: ${typeof myArray}`);
+
+
+// Only change code below this line
