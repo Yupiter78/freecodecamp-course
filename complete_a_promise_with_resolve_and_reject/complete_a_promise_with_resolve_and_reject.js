f29000c504edb4231e46d3bf1051abd09627fd05
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2022-11-19 21:51:59 +0300

    feat: add task complete a promise with resolve and reject

---
+++ complete_a_promise_with_resolve_and_reject/complete_a_promise_with_resolve_and_reject.js
@@ -0,0 +1,13 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer = Math.round(Math.random());
+    console.log({responseFromServer: responseFromServer});
+
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+console.log(makeServerRequest);
